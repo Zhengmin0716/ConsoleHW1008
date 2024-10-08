78570532f378eb99485b5fe2a9fb4d288e16ae15
王政閔 <min10qq@gmail.com>
2024-10-09 00:24:32 +0800

    更新乘法表格式並改進代碼風格
    - 在 `#include <iostream>` 前添加不可見字符
    - 在 `#include <iostream>` 後添加 `using namespace std;`
    - 修改 `printMultiplicationTable` 函數的輸出格式
    - 刪除 `printMultiplicationTable` 函數內的註解
    - 將 `main` 函數內變數 `n` 初始化為 `int n = 1;

--- ConsoleHW1008/ConsoleHW1008.cpp
+++ ConsoleHW1008/ConsoleHW1008.cpp
@@ -1,21 +1,18 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 using namespace std;
 
 void printMultiplicationTable(int n) {
-    // 迴圈遍歷行
     for (int i = 1; i <= n; ++i) {
-        // 迴圈遍歷列
         for (int j = 1; j <= n; ++j) {
-            // 輸出乘法結果，並用tab分隔
-            cout << i * j << "\t";
+            cout << i << "*" << j << "=" << i * j << "\t";
         }
-        // 每行結束後換行
         cout << endl;
     }
 }
 
 int main() {
-    int n;
+    int n = 1;
     cout << "請輸入乘法表的大小: ";
     cin >> n;
     printMultiplicationTable(n);
